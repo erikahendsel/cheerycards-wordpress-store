bd11b39d9a78a1b750bd7479193a6f354e607edb
Your Name <erikahendsel@hotmail.com>
2021-01-15 15:41:44 +0100

    Hero image created. ACF added to the project.-001 & -002

--- front-page.php
+++ front-page.php
@@ -1,15 +1,21 @@
 <?php get_header(); ?>
 
 <div class="container">
+    <!-- <h1><?php the_title(); ?></h1> -->
 
-    <h1><?php the_title(); ?></h1>
-
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <!-- <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
             <?php the_content(); ?>
 
     <?php endwhile;
-    endif; ?>
+            endif; ?> -->
+
+    <!-- Hero image -->
+    <?php if (has_post_thumbnail()) : ?>
+        <div class='hero-img'>
+            <?php the_post_thumbnail(); ?>
+        </div>
+    <?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
