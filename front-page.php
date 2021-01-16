06c8df2f7ef47d625abe0ccba59b5e4bbfc118f5
Your Name <erikahendsel@hotmail.com>
2021-01-16 21:16:56 +0100

    Flash section added.-003

--- front-page.php
+++ front-page.php
@@ -16,6 +16,40 @@
             <?php the_post_thumbnail(); ?>
         </div>
     <?php endif; ?>
+
+    <!-- Flash section ie news/sales/information block -->
+    <section class='flash'>
+        <div class="flash-container">
+            <div class="flash-block flash-one">
+                <?php
+                $image = get_field('flash_image_first');
+                if (!empty($image)) : ?>
+                    <img class='flash-img' src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endif; ?>
+
+                <?php
+                $link = get_field('flash_link_button_first');
+                if ($link) : ?>
+                    <a class="button" href="<?php echo esc_url($link); ?>">Shop now</a>
+                <?php endif; ?>
+            </div>
+            <div class="flash-block flash-two">
+                <?php
+                $image = get_field('flash_image_second');
+                if (!empty($image)) : ?>
+                    <img class='flash-img' src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endif; ?>
+
+                <?php
+                $link = get_field('flash_link_button_second');
+                if ($link) : ?>
+                    <a class="button" href="<?php echo esc_url($link); ?>">Shop now</a>
+                <?php endif; ?>
+            </div>
+
+        </div>
+    </section>
+
 </div>
 
 <?php get_footer(); ?>
